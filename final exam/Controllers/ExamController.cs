c2cb10ec47cc4d7166c15a932897a709359ddaf1
陳琦旻 <calvm0421@gmail.com>
2021-06-24 20:33:46 +0800

    final

---
+++ final exam/Controllers/ExamController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final_exam.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult week()
+        {
+            return View();
+        }
+    }
+}
---
+++ final exam/Models/Partials/News.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace final_exam.Models
+{
+    [MetadataType(typeof(NewsMetadata))]
+
+    public partial class News
+    {
+    }
+    public class NewsMetadata
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "欄位 標題 必須是最大長度為50的字串")]
+        [Display(Name = "標題")]
+        [StringLength(50, MinimumLength = 0, ErrorMessage = "欄位 標題 必須是最大長度為50的字串")]
+
+        public string Title { get; set; }
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "內容")]
+        public string Content { get; set; }
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "發布日期")]
+        public System.DateTime PublishDate { get; set; }
+    }
+
+}
